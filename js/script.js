e605676839682419db8ba2a9f26cb0060daa3b60
Carlos Antonio de Souza Galdino Junior <carlosgaldino90@gmail.com>
2022-08-04 21:18:28 -0300

    TESTE

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-permissions-policy: interest-cohort=();
+
 const botaoMenu = document.querySelector('.cabecalho__menu');
 const menu = document.querySelector('.menu-lateral');
 botaoMenu.addEventListener('click', ()=>{
