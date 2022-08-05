bc1a2a0afd4418c22746ee7b71ad85d31cb83dd9
Carlos Antonio de Souza Galdino Junior <carlosgaldino90@gmail.com>
2022-08-04 21:06:57 -0300

    Tteste

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+permissions-policy: interest-cohort=();
 const botaoMenu = document.querySelector('.cabecalho__menu');
 const menu = document.querySelector('.menu-lateral');
 botaoMenu.addEventListener('click', ()=>{
     menu.classList.toggle('menu-lateral--ativo');
 });
+
